3a7be0596b757fccbee780ce98143e12f96b4d5c
sanika2309 <sanikamhatre003@gmail.com>
2024-10-23 19:55:41 +0530

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,48 @@
+from PIL import Image
+
+def encrypt_image(input_path, output_path, key):
+    img = Image.open(input_path)
+    pixels = img.load()
+
+    width, height = img.size
+
+    for i in range(width):
+        for j in range(height):
+            r, g, b = pixels[i, j]
+
+            # swapping red and blue channels
+            encrypted_pixel = (b, g, r)
+
+            pixels[i, j] = encrypted_pixel
+
+    img.save(output_path)
+    print("Image encrypted successfully!")
+
+def decrypt_image(input_path, output_path, key):
+    img = Image.open(input_path)
+    pixels = img.load()
+
+    width, height = img.size
+
+    for i in range(width):
+        for j in range(height):
+            r, g, b = pixels[i, j]
+
+            # swapping red and blue channels back
+            decrypted_pixel = (b, g, r)
+
+            pixels[i, j] = decrypted_pixel
+
+    img.save(output_path)
+    print("Image decrypted successfully!")
+
+ # image path
+input_image = r"C:\Users\Sanika Mhatre\Downloads\PRODIGY_CS_02-main\PRODIGY_CS_02-main\input_image.jpg"
+encrypted_image = r"C:\Users\Sanika Mhatre\Downloads\PRODIGY_CS_02-main\PRODIGY_CS_02-main\encrypted_image.jpg"
+decrypted_image = r"C:\Users\Sanika Mhatre\Downloads\PRODIGY_CS_02-main\PRODIGY_CS_02-main\decrypted_image.jpg"
+
+# Encrypt the image
+encrypt_image(input_image, encrypted_image, key=None)
+
+# Decrypt the image
+decrypt_image(encrypted_image, decrypted_image, key=None)
